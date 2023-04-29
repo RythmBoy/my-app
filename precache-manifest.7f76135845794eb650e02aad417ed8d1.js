2ef8f4dee971e9bdd14bf55e104c8f93be8749a1
Edgar Loya <sonydevice@live.com>
2023-04-29 02:37:59 -0700

    Updates

---
+++ precache-manifest.7f76135845794eb650e02aad417ed8d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "57118c02986fe8f130a0",
+    "url": "/my-app/static/css/main.e308272a.chunk.css"
+  },
+  {
+    "revision": "57118c02986fe8f130a0",
+    "url": "/my-app/static/js/main.57118c02.chunk.js"
+  },
+  {
+    "revision": "927bd4bacd441db6510d",
+    "url": "/my-app/static/js/1.927bd4ba.chunk.js"
+  },
+  {
+    "revision": "f12c163cefac7b327e82",
+    "url": "/my-app/static/js/2.f12c163c.chunk.js"
+  },
+  {
+    "revision": "b72ddd22de18b552adee",
+    "url": "/my-app/static/js/runtime~main.b72ddd22.js"
+  },
+  {
+    "revision": "dc6fed34e14e670d51120ece4c0cde29",
+    "url": "/my-app/static/media/people-and-animals-image.dc6fed34.png"
+  },
+  {
+    "revision": "c1e03c0bcefaa8720dc26b68e0aaa19f",
+    "url": "/my-app/index.html"
+  }
+];
